1a70110b33f08a1d31182cbb52527e57107c1119
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2023-09-10 21:51:01 +0900

    [Bronze V] Title: 개수 세기, Time: 128 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int target = Integer.parseInt(br.readLine());
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == target) {
+                ans++;
+            }
+        }
+
+        System.out.println(ans);
+
+
+
+
+
+    }
+
+
+
+
+
+}
