c4de8c390e9d163ccd6897e2663809d4b369b29c
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-03-15 12:45:34 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,15 @@
+import sys
+from itertools import combinations
+
+n,m = map(int, sys.stdin.readline().rstrip().split())
+
+arr = []
+
+for i in range(1, n+1):
+    arr.append(i)
+
+ans = list(combinations(arr,m))
+
+for a in ans:
+    data = list(a)
+    print(" ".join(map(str, data)))
