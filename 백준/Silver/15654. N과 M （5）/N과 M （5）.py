75b15dea94de5eaa7c00d70a520675dbc97009c2
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-03-15 16:08:51 +0900

    [Silver III] Title: N과 M (5), Time: 136 ms, Memory: 35556 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,19 @@
+import sys
+from itertools import *
+
+n,m = map(int, sys.stdin.readline().rstrip().split())
+
+arr = []
+
+data = map(int, sys.stdin.readline().rstrip().split())
+
+for i in data:
+    arr.append(i)
+
+arr.sort()
+
+arr2 = list(permutations(arr,m))
+
+for i in arr2:
+    k = list(i)
+    print(" ".join(map(str, i)))
