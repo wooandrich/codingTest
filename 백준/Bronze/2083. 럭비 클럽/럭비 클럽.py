e46674df0ff4bc8a6ceafcec7070da55c8a54f77
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-03-22 11:44:53 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.py
@@ -0,0 +1,9 @@
+while 1:
+    name, age, weight = input().split()
+    if name == '#':
+        break
+
+    if int(age) > 17 or int(weight) >= 80:
+        print(name, "Senior")
+    else:
+        print(name, "Junior")
