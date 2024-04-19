063b2626ee66d8fccae2e1066a8f56e1c12f7688
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-04-19 22:49:15 +0900

    [Bronze I] Title: 최소공배수, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -0,0 +1,18 @@
+import sys
+import math
+
+for tc in range(int(sys.stdin.readline().rstrip())):
+    a,b = map(int, sys.stdin.readline().rstrip().split())
+    print(math.lcm(a,b))
+
+
+
+
+
+
+
+
+
+
+
+
