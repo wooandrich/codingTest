20e13f991363e3fe5bc204a2fe9c58bc17455306
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-04-30 21:19:35 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 528 ms, Memory: 169216 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,14 +1,24 @@
 import sys
 
 s = sys.stdin.readline().rstrip()
+cnt = [[0 for _ in range(26)] for _ in range(len(s))]
+cnt[0][ord(s[0])-97] = 1
+for i in range(1,len(s)):
+    cnt[i][ord(s[i]) - 97] = 1
+    for j in range(26):
+        cnt[i][j] += cnt[i-1][j]
+
 
 q = int(sys.stdin.readline().rstrip())
 
 for tc in range(q):
-    a,l,r = sys.stdin.readline().rstrip().split()
+    alpha,l,r = sys.stdin.readline().rstrip().split()
     l = int(l)
     r = int(r)
 
-    temp = s[l:r+1]
-    print(temp.count(a))
+    if l > 0:
+        result = cnt[r][ord(alpha)-97] - cnt[l-1][ord(alpha)-97]
+    else:
+        result = cnt[r][ord(alpha)-97]
+    print(result)
 
