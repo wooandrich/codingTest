0310043fddc759eb98d9980c43fa8226976b0ab3
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-05-18 16:40:06 +0900

    [Gold III] Title: 소수의 연속합, Time: 540 ms, Memory: 161504 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -0,0 +1,34 @@
+import sys
+import math
+
+
+input = sys.stdin.readline
+
+n = int(input())
+
+prime = [True for i in range(n+1)]
+prime[1] = False
+
+for i in range(2, int(math.sqrt(n) + 1)):
+    if prime[i]:
+        j = 2
+        while i*j <= n:
+            prime[i*j] = False
+            j += 1
+arr = []
+for i in range(2,n+1):
+    if prime[i]:
+        arr.append(i)
+
+prefix_sum = 0
+end = 0
+ans = 0
+
+for start in range(len(arr)):
+    while prefix_sum < n and end < len(arr):
+        prefix_sum += arr[end]
+        end += 1
+    if prefix_sum == n:
+        ans += 1
+    prefix_sum -= arr[start]
+print(ans)
