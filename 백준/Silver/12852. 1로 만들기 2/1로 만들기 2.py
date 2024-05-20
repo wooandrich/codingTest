198cf3cb7636311ff4e7d76ed229e1b646dfca97
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-05-20 22:58:39 +0900

    [Silver I] Title: 1로 만들기 2, Time: 2312 ms, Memory: 379336 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
@@ -1,31 +1,29 @@
 import sys
-from collections import deque
 
 input = sys.stdin.readline
 
 n = int(input())
 
-q = deque()
-q.append([n])
 
-ans = []
+dp = [[0, []] for _ in range(n+1)]
 
+dp[1][0] = 0
+dp[1][1] = [1]
 
-while q:
-    temp = q.popleft()
-    x = temp[0]
-    if x == 1:
-        ans = temp
-        break
+for i in range(2,n+1):
+    dp[i][0] = dp[i-1][0] + 1
+    dp[i][1] = dp[i-1][1] + [i]
 
-    if x % 3 == 0:
-        q.append([x//3] + temp)
-    if x % 2 == 0:
-        q.append([x//2] + temp)
-    q.append([x-1] + temp)
-print(len(ans)-1)
-print(*ans[::-1])
+    if i % 3 == 0 and dp[i//3][0] + 1 < dp[i][0]:
+        dp[i][0] = dp[i//3][0] + 1
+        dp[i][1] = dp[i//3][1] + [i]
 
+    if i % 2 == 0 and dp[i//2][0] + 1 < dp[i][0]:
+        dp[i][0] = dp[i//2][0] + 1
+        dp[i][1] = dp[i//2][1] + [i]
+
+print(dp[n][0])
+print(*dp[n][1][::-1])
 
 
 
