0bca5f294b32355370b0c2f5468ade14798e3ef8
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-05-21 16:51:25 +0900

    [Gold IV] Title: LCS 2, Time: 756 ms, Memory: 427616 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -0,0 +1,44 @@
+import sys
+
+a = ' ' + sys.stdin.readline().rstrip()
+b = ' ' + sys.stdin.readline().rstrip()
+
+
+dp = [["" for _ in range(len(b))] for _ in range(len(a))]
+
+for i in range(1, len(a)):
+    for j in range(1, len(b)):
+        if a[i] == b[j]:
+            dp[i][j] = dp[i-1][j-1] + a[i]
+        else:
+            if len(dp[i-1][j]) >= len(dp[i][j-1]):
+                dp[i][j] = dp[i-1][j]
+            else:
+                dp[i][j] = dp[i][j-1]
+
+find = dp[-1][-1]
+
+print(len(find))
+print(find)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
