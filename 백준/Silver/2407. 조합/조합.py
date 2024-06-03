b16ab63c1aa25b2d6931adf03def9b0b8e355f75
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-06-03 21:03:34 +0900

    [Silver III] Title: 조합, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2407. 조합/조합.py
@@ -0,0 +1,16 @@
+import sys
+from itertools import combinations
+
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+
+a = 1
+b = 1
+
+for i in range(m):
+    a *= (n-i)
+    b *= (1+i)
+
+
+print(a//b)
