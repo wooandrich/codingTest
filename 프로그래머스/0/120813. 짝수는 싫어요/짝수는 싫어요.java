6c875f50725b558b35d826b308d0106e62511bd7
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-08-12 16:37:57 +0900

    [level 0] Title: 짝수는 싫어요, Time: 13.88 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> arr = new ArrayList();
+        
+        for (int i=1;i<=n;i+=2) arr.add(i);
+        return arr.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
