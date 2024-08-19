7a8b4d8691df00b92ed48eefdad2c0d01c0e079b
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-08-19 09:19:06 +0900

    [Gold V] Title: 최소비용 구하기, Time: 260 ms, Memory: 57372 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -0,0 +1,36 @@
+import sys
+import heapq
+INF = int(1e9)
+
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+
+graph = [[] for i in range(n + 1)]
+distance = [INF] * (n + 1)
+
+for _ in range(m):
+    a,b,c = map(int, input().split())
+    graph[a].append((b,c))
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+
+        if distance[now] < dist:
+            continue
+
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+start, end = map(int, input().split())
+dijkstra(start)
+
+print(distance[end])
