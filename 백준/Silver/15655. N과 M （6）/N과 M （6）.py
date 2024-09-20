7546e65ce586064c8b1bcdf7a5533c6e4c301fd2
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-09-20 16:31:26 +0900

    [Silver III] Title: N과 M (6), Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,26 @@
+import sys
+
+input = sys.stdin.readline
+
+def recur(number):
+    if number == m:
+        print(*arr)
+        return
+
+    for i in k:
+        if i in arr:
+            continue
+        if len(arr) > 0:
+            temp = arr[-1]
+            if i < temp:
+                continue
+        arr.append(i)
+        recur(number + 1)
+        arr.pop()
+
+n,m = map(int, input().split())
+k = sorted(list(map(int, input().split())))
+arr = []
+
+recur(0)
+
