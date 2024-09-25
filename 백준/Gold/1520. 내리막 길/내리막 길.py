8c0c48ac9d89bc6c55b9df7d6a0474f4fe1ed57d
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-09-25 21:37:26 +0900

    [Gold III] Title: 내리막 길, Time: 108 ms, Memory: 48000 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,30 +1,31 @@
 import sys
-sys.setrecursionlimit(10**9)
+sys.setrecursionlimit(1000000000)
+input = sys.stdin.readline
 
-m,n = map(int,sys.stdin.readline().rstrip().split())
+def recur(x,y):
 
-arr = [list(map(int,sys.stdin.readline().rstrip().split())) for _ in range(m)]
-dp = [[-1 for _ in range(n)] for _ in range(m)]
-dx = [1,-1,0,0]
-dy = [0,0,-1,1]
-
-
-
-
-
-def dfs(row,col):
-    global ans
-    if row == m-1 and col == n-1:
+    if x == m-1 and y == n-1:
         return 1
 
-    if dp[row][col] == -1:
-        dp[row][col] = 0
+    if dp[x][y] != -1:
+        return dp[x][y]
 
-        for i in range(4):
-            nx = row + dx[i]
-            ny = col + dy[i]
-            if 0<=nx<m and 0<=ny<n:
-                if arr[nx][ny] < arr[row][col]:
-                    dp[row][col] += dfs(nx,ny)
-    return dp[row][col]
-print(dfs(0,0))
+    route = 0
+    for dx,dy in [[-1,0],[1,0],[0,-1],[0,1]]:
+        nx = x+dx
+        ny = y+dy
+        if 0 <= nx < m and 0 <= ny < n:
+            if graph[x][y] > graph[nx][ny]:
+                route += recur(nx,ny)
+    dp[x][y] = route
+    return dp[x][y]
+
+
+m,n = map(int,input().split())
+
+graph = [list(map(int,input().split())) for _ in range(m)]
+
+dp = [[-1 for _ in range(n)] for _ in range(m)]
+
+ans = recur(0,0)
+print(ans)
