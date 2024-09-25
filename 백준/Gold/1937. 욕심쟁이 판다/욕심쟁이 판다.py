14aa67501fe79c435530b2468ecb9930ddefd456
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-09-25 21:45:14 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 832 ms, Memory: 52076 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
@@ -4,15 +4,17 @@ input = sys.stdin.readline
 
 def recur(x,y):
 
-    if dp[x][y] != 0:
+    if dp[x][y] != -1:
         return dp[x][y]
 
+    route = 0
     for dx,dy in [[-1,0],[1,0],[0,-1],[0,1]]:
         nx = x+dx
         ny = y+dy
         if 0<=nx<n and 0<=ny<n:
             if graph[x][y] < graph[nx][ny]:
-                dp[x][y] = max(dp[x][y], recur(nx,ny) + 1)
+                route = max(route, recur(nx,ny) + 1)
+    dp[x][y] = route
     return dp[x][y]
 
 
@@ -20,7 +22,7 @@ n = int(input())
 
 graph = [list(map(int, input().split())) for _ in range(n)]
 
-dp = [[0 for _ in range(n)] for _ in range(n)]
+dp = [[-1 for _ in range(n)] for _ in range(n)]
 
 for x in range(n):
     for y in range(n):
