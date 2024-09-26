62a67b161458960ee87f5e7fb04727aee58684aa
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-09-26 15:08:43 +0900

    [Gold V] Title: LCS, Time: 496 ms, Memory: 56528 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,16 +1,16 @@
-str1 = input()
-str2 = input()
+import sys
 
-n = len(str1)
-m = len(str2)
+input = sys.stdin.readline
 
-dp = [[0] * (m+1) for _ in range(n+1)]
+m = input().strip()
+n = input().strip()
 
-for i in range(1, n+1):
-    for j in range(1,m+1):
-        if str1[i-1] == str2[j-1]:
+dp = [[0 for _ in range(len(n)+1)]for _ in range(len(m)+1)]
+
+for i in range(1, len(m) + 1):
+    for j in range(1, len(n) + 1):
+        if m[i-1] == n[j-1]:
             dp[i][j] = dp[i-1][j-1] + 1
         else:
             dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-print(dp[n][m])
-
+print(dp[len(m)][len(n)])
