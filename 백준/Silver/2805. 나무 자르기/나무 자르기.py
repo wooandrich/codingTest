9695e0dba3e3fca80d9cae36ccc36e3e5550c6ed
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-09-28 17:18:18 +0900

    [Silver II] Title: 나무 자르기, Time: 3940 ms, Memory: 150200 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,28 +1,24 @@
 import sys
 
-n,m = map(int, sys.stdin.readline().rstrip().split())
-arr = list(map(int, sys.stdin.readline().rstrip().split()))
+input = sys.stdin.readline
 
+n,target = map(int,input().split())
 
+arr = sorted(list(map(int,input().split())))
 
-start = 0
-end = max(arr)
-result = 0
-while start <= end:
-    total = 0
-    mid = (start + end) // 2
+s = 0
+e = arr[-1]
+ans = 0
+while s <= e:
+    mid = (s+e)//2
+    cnt = 0
 
     for i in arr:
-        if mid < i:
-            total += i - mid
-    # 떡 양이 부족한 경우 더 많이 자르기
-    if total < m:
-        end = mid - 1
-    # 떡 양이 충분한 경우
+        if i > mid:
+            cnt += i - mid
+
+    if cnt < target:
+        e = mid - 1
     else:
-        result = mid # 최대한 덜 잘랐을 때가 정답이므로, 여기서 기록
-        start = mid + 1
-
-print(result)
-
-
+        s = mid + 1
+print(e)
