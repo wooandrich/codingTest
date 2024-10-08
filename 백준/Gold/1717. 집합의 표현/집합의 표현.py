11dbfdb012663d6b2e7bec63272f2095e5b4f7d2
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-10-08 21:07:53 +0900

    [Gold V] Title: 집합의 표현, Time: 288 ms, Memory: 71112 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -7,21 +7,14 @@ def _union(a, b):
     a = _find(a)
     b = _find(b)
 
-    if a == b:
-        return
+    parent[b] = a
 
-    if rank[a] < rank[b]:
-        parent[a] = b
-    elif rank[a] > rank[b]:
-        parent[b] = a
-    else:
-        parent[a] = b
-        rank[b] += 1
 
 def _find(a):
     if parent[a] == a:
         return a
     else:
+        parent[a] = _find(parent[a])
         return _find(parent[a])
 
 
@@ -30,7 +23,6 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 
 parent = [i for i in range(n + 1)]
-rank = [0 for i in range(n + 1)]
 
 for _ in range(m):
     x, a, b = map(int, input().split())
