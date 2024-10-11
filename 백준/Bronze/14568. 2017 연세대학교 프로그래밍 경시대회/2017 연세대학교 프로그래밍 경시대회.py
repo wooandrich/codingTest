b2ee66660b20b2a57c5d2f37d90577de4b9dea7b
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-10-11 14:19:14 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 116 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.py
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.py
@@ -6,12 +6,12 @@ n = int(input())
 
 ans = 0
 
-for a in range(n+1):
-    for b in range(n+1):
-        for c in range(n+1):
-            if a + b + c == n:
-                if a >= b + 2:
-                    if a > 0 and b > 0 and c > 0:
-                        if c % 2 == 0:
+for i in range(0,n+1):
+    for j in range(0,n+1):
+        for k in range(0,n+1):
+            if i + j + k == n:
+                if k >= j + 2:
+                    if i % 2 == 0:
+                        if i > 0 and j > 0 and k > 0:
                             ans += 1
 print(ans)
