bcf6b4d470b341b2a215e237300d7b7ee87cc49f
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-10-11 15:00:50 +0900

    [Silver III] Title: 숫자 야구, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.py
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -3,41 +3,41 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-
-hint = [list(map(int, input().split())) for _ in range(n)]
+arr = [list(map(int, input().split())) for _ in range(n)]
 ans = 0
 
-for a in range(1,10):
-    for b in range(1,10):
-        for c in range(1,10):
-            if a == b or b == c or a == c:
-                continue
-            cnt = 0
-            for arr in hint:
-                number = arr[0]
-                strike = arr[1]
-                ball = arr[2]
+for a in range(1, 10):
+    for b in range(1, 10):
+        for c in range(1, 10):
+            if a != b and a != c and b != c:
+                cnt = 0
+                for condi in arr:
+                    num = condi[0]
+                    strike = condi[1]
+                    ball = condi[2]
+                    s = 0
+                    k = 0
+                    p = num // 100
+                    q = num % 100 // 10
+                    r = num % 100 % 10
+                    if a == p:
+                        s += 1
+                    elif a == q or a == r:
+                        k += 1
+                    if b == q:
+                        s += 1
+                    elif b == p or b == r:
+                        k += 1
+                    if c == r:
+                        s += 1
+                    elif c == p or c == q:
+                        k += 1
 
-                strike_cnt = 0
-                ball_cnt = 0
-
-                if a == number // 100:
-                    strike_cnt += 1
-                elif str(a) in str(number):
-                    ball_cnt += 1
-
-                if b == number % 100 // 10:
-                    strike_cnt += 1
-                elif str(b) in str(number):
-                    ball_cnt += 1
-
-                if c == number % 100 % 10:
-                    strike_cnt += 1
-                elif str(c) in str(number):
-                    ball_cnt += 1
-
-                if strike == strike_cnt and ball == ball_cnt:
-                    cnt += 1
-            if cnt == n:
-                ans += 1
+                    if strike == s and ball == k:
+                        cnt += 1
+                    else:
+                        break
+                if cnt == n:
+                    ans += 1
 print(ans)
+
