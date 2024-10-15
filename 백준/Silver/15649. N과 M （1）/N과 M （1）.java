f291278929b48e1d9685fec0a49e871bae0a4d45
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-10-16 01:37:12 +0900

    [Silver III] Title: N과 M (1), Time: 1636 ms, Memory: 64632 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n,m;
+    static List<Integer> arr = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        recur(0);
+
+
+
+
+
+
+
+    }
+
+    static void recur(int num) {
+        if (num == m) {
+            arr.stream().forEach(x -> System.out.print(x + " "));
+            System.out.println();
+            return;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            if (arr.contains(i)) {
+                continue;
+            }
+            arr.add(i);
+            recur(num + 1);
+            arr.remove(arr.size() - 1);
+        }
+
+    }
+}
