7acf0ede8f334b180782f8a262770a6d59e0671c
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-10-16 22:03:23 +0900

    [level 2] Title: 의상, Time: 0.61 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,16 +1,17 @@
 import java.util.*;
+
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
-        HashMap<String, Integer> map = new HashMap<>();
         
-        for (int i=0;i<clothes.length;i++) {
-            String a = clothes[i][1];
-            map.put(a, map.getOrDefault(a, 0) + 1);
+        HashMap<String, Integer> hm = new HashMap<>();
+        
+        for (String[] c : clothes) {
+            hm.put(c[1], hm.getOrDefault(c[1], 0) + 1);
         }
         
-        for (String key : map.keySet()) {
-            answer *= (map.get(key) + 1);
+        for (String s : hm.keySet()) {
+            answer *= (hm.get(s) + 1);
         }
         
         return answer - 1;
