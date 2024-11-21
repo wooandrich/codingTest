688f1e8d4e387cddc340888bc13129c9b05ca524
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-11-21 15:56:51 +0900

    [Bronze V] Title: A-B, Time: 72 ms, Memory: 12184 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.kt
+++ 백준/Bronze/1001. A－B/A－B.kt
@@ -5,8 +5,8 @@ import java.util.StringTokenizer
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val st = StringTokenizer(readLine())
 
-    val a = Integer.parseInt(st.nextToken())
-    val b = Integer.parseInt(st.nextToken())
+    val a = st.nextToken().toInt()
+    val b = st.nextToken().toInt()
 
     print(a - b)
 
