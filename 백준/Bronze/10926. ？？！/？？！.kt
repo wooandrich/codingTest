3810aad3b10b05f2660c5f49cea7dc5442fd72ca
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-11-21 16:04:23 +0900

    [Bronze V] Title: ??!, Time: 80 ms, Memory: 12412 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.kt
@@ -0,0 +1,12 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+
+    val name = readln()
+
+    println(name + "??!")
+
+
+}
