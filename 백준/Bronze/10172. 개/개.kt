9a0263858f54f4822636510d9e3fbe795edc1e57
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-11-21 16:18:54 +0900

    [Bronze V] Title: 개, Time: 68 ms, Memory: 11712 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.kt
@@ -0,0 +1,14 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+
+    println("|\\_/|\n" +
+            "|q p|   /}\n" +
+            "( 0 )\"\"\"\\\n" +
+            "|\"^\"`    |\n" +
+            "||_/=\\\\__|")
+
+
+}
