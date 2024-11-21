c81eb4ca73be41c0b593696edd4d94b04640f67c
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-11-21 16:22:37 +0900

    [Bronze V] Title: 시험 성적, Time: 80 ms, Memory: 12184 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.kt
@@ -0,0 +1,20 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+
+    val n = readLine().toInt()
+
+    when (n) {
+        in 90..100 -> println("A")
+        in 80..89 -> println("B")
+        in 70..79 -> println("C")
+        in 60..69 -> println("D")
+        else -> println("F")
+    }
+
+
+
+
+}
