56ce97c2a08ce45a4f96ddd489bfd385083d7ce7
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-11-21 16:06:22 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 72 ms, Memory: 12192 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.kt
@@ -0,0 +1,11 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+
+    val n = readLine().toInt()
+
+    println(n - 543)
+
+}
