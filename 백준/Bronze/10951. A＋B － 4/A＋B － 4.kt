bebdd7009f6ad847a67e210df8d1b0ee3b9a0262
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-11-22 16:32:29 +0900

    [Bronze V] Title: A+B - 4, Time: 88 ms, Memory: 12328 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.kt
@@ -0,0 +1,21 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+
+
+    while (true) {
+        val st = StringTokenizer(readLine() ?: break)
+
+        if (st.countTokens() < 2) break
+
+        val a = st.nextToken().toInt()
+        val b = st.nextToken().toInt()
+
+
+
+        println(a + b)
+    }
+
+}
