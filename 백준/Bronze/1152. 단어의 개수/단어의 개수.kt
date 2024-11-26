87053977eb2ffc658122859e2438af260765fe65
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-11-26 14:32:43 +0900

    [Bronze II] Title: 단어의 개수, Time: 176 ms, Memory: 20892 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.kt
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.kt
@@ -5,15 +5,7 @@ import kotlin.math.max
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
 
-    val st = StringTokenizer(readLine())
-
-    var arr = ArrayList<String>()
-
-    while (st.hasMoreTokens()) {
-        val token = st.nextToken()
-        arr.add(token)
-    }
-    println(arr.size)
+    println(StringTokenizer(readLine()).countTokens())
 
 
 
