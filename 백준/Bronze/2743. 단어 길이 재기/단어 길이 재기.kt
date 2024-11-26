be4ad52b2ee5eb33036731657a7d5feca22b2d66
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-11-26 13:57:47 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 72 ms, Memory: 11904 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.kt
@@ -0,0 +1,21 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+import kotlin.math.max
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+
+    val s = readLine()
+
+
+    println(s.length)
+
+
+
+
+
+
+
+
+
+}
