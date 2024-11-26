72d8f501a17c10086f51b85b3c883ce2e557827d
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-11-26 15:34:31 +0900

    [Bronze V] Title: 새싹, Time: 76 ms, Memory: 11684 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.kt
@@ -0,0 +1,22 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+import kotlin.math.max
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+
+    println("         ,r'\"7\n" +
+            "r`-_   ,'  ,/\n" +
+            " \\. \". L_r'\n" +
+            "   `~\\/\n" +
+            "      |\n" +
+            "      |")
+
+
+
+
+
+
+
+
+}
