73625ea6c41ef2568c2a859e66489f6fc93ba5df
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-11-26 14:07:25 +0900

    [Bronze V] Title: 아스키 코드, Time: 92 ms, Memory: 14400 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.kt
@@ -0,0 +1,20 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+import kotlin.math.max
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+
+    val s = readLine().first().code
+
+    println(s)
+
+
+
+
+
+
+
+
+
+}
