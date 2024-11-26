85ac8e4fa4b02e87c6b6a63882cea62ae918aa62
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-11-26 15:32:48 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 84 ms, Memory: 12876 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.kt
@@ -0,0 +1,21 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+import kotlin.math.max
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+
+    while (true) {
+        val s = readLine() ?: break
+        
+        println(s)
+    }
+
+
+
+
+
+
+
+
+}
