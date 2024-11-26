029093ea7401cd61f602f2a83580b3886db9f6a1
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-11-26 15:51:11 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 96 ms, Memory: 14460 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.kt
@@ -0,0 +1,20 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+
+    val s = readLine()
+
+    if (s == s.reversed()) println(1) else println(0)
+
+
+
+
+
+
+
+
+
+
+}
