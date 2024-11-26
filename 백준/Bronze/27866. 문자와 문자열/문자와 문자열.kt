c9a9ba6ed9573013a0b0b24454d1e1baca493215
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-11-26 13:57:03 +0900

    [Bronze V] Title: 문자와 문자열, Time: 76 ms, Memory: 12160 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.kt
@@ -0,0 +1,22 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+import kotlin.math.max
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+
+    val s = readLine()
+
+    val n = readLine().toInt()
+
+    println(s[n-1])
+
+
+
+
+
+
+
+
+
+}
