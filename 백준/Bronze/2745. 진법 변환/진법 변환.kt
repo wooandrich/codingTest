30501df279a83c1a7c09cf54e410597067d8e139
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2024-12-12 16:00:49 +0900

    [Bronze II] Title: 진법 변환, Time: 108 ms, Memory: 15236 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.kt
@@ -0,0 +1,21 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+
+
+    val (n, b) = readLine().split(" ")
+
+    println(n.toInt(b.toInt()))
+
+
+
+
+
+
+
+
+
+
+}
