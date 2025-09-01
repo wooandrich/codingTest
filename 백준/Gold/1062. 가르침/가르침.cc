839857d53311431fd14b650ff553666fc0c4a354
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2025-09-01 17:48:37 +0900

    [Gold IV] Title: 가르침, Time: 140 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1062. 가르침/가르침.cc
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n,m, words[51];
+string s;
+
+int count(int mask) {
+    int cnt = 0;
+    for (int word : words) {
+        if (word && (word & mask) == word) cnt++;
+    }
+    return cnt;
+}
+
+int go(int index, int k, int mask) {
+    if (k < 0) return 0;
+    if (index == 26) return count(mask);
+    int ret = go(index + 1, k- 1, mask | (1 << index));
+    if (index != 'a'-'a' && index != 'n'-'a' && index != 't'-'a' && index != 'i'-'a' && index != 'c'-'a') {
+        ret = max(ret, go(index+1, k, mask));
+    }
+    return ret;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+    cin >> n >> m;
+
+    for (int i=0;i<n;i++) {
+        cin >> s;
+        for (char str : s) {
+            words[i] |= 1 << (str - 'a');
+        }
+    }
+
+    cout << go(0,m,0) << '\n';
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+
+
