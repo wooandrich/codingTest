251e487e2eea610ac2b1c8ec7c009693b28b443a
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2025-09-01 15:20:24 +0900

    [Gold IV] Title: 알파벳, Time: 372 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -2,56 +2,47 @@
 using namespace std;
 
 const int MAX = 24;
+const int INF = 987654321;
+int r,c,ret=0;
+int dy[] = {-1,1,0,0};
+int dx[] = {0,0,-1,1};
+
 int a[MAX][MAX];
-int visited[MAX][MAX];
-int r,c,ret;
-int dy[] = {-1, 1, 0, 0};
-int dx[] = {0, 0, -1, 1};
-int check[26];
-string s;
 
-void dfs(int y, int x, int cnt) {
-
+void go(int y, int x, int num, int cnt) {
     ret = max(ret, cnt);
 
     for (int i=0;i<4;i++) {
         int ny = y + dy[i];
         int nx = x + dx[i];
-        int node = a[ny][nx] - 'A';
 
         if (ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
-        if (check[node]) continue;
-        if (visited[ny][nx]) continue;
-
-        visited[ny][nx] = 1;
-        check[node] = 1;
-        dfs(ny,nx,cnt + 1);
-        visited[ny][nx] = 0;
-        check[node] = 0;
+        int _next = 1 << (a[ny][nx] - 'A');
+        if ((num & _next) == 0) go(ny, nx, num | _next, cnt + 1);
     }
-
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    cin >> r >> c;
+    cin >>  r >> c;
 
     for (int i=0;i<r;i++) {
+        string s;
         cin >> s;
-
         for (int j=0;j<c;j++) {
             a[i][j] = s[j];
         }
     }
 
-    visited[0][0] = 1;
-    int start = a[0][0] - 'A';
-    check[start] = 1;
-    dfs(0, 0, 1);
+    go(0,0, 1 << (int)(a[0][0] - 'A'), 1);
 
     cout << ret << '\n';
+
+
+
+
 
 
 
