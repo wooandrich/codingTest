0777af4e5dec347a191e2a176a106cfd57caf9b9
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2025-09-07 19:00:30 +0900

    [Gold IV] Title: Tree, Time: 0 ms, Memory: 2184 KB -BaekjoonHub

---
+++ 백준/Gold/13244. Tree/Tree.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t, n, m, a, b, visited[1004], cnt;
+vector<int> adj[1004];
+
+void dfs(int here) {
+    visited[here] = 1;
+    for (int there : adj[here]){
+        if (!visited[there]) dfs(there);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> t;
+
+    while(t--) {
+        for (int i=0;i<1004;i++) adj[i].clear();
+        fill(visited, visited + 1004, 0);
+
+        cnt = 0;
+        cin >> n >> m;
+
+        for (int i=0;i<m;i++) {
+            cin >> a >> b;
+            adj[b].push_back(a);
+            adj[a].push_back(b);
+        }
+        for (int i=1;i<=n;i++) {
+            if (!visited[i]){
+                dfs(i);
+                cnt++;
+            }
+        }
+        if (m == n-1 && cnt == 1) puts("tree");
+        else puts("graph");
+    }
+
+
+
+
+
+
+
+    
+
+    return 0;
+}
