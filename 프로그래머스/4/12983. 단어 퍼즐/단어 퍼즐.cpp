c6990f20a9b062a5d0e3579178073c16cb319ba2
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2025-10-24 21:59:24 +0900

    [level 4] Title: 단어 퍼즐, Time: 23.69 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/4/12983. 단어 퍼즐/단어 퍼즐.cpp
+++ 프로그래머스/4/12983. 단어 퍼즐/단어 퍼즐.cpp
@@ -2,37 +2,26 @@
 using namespace std;
 
 int dp[20004], INF = 1e9;
-vector<string> strs;
-string t;
 
-bool check(int n, string &s) {
-    for (int i=0;i<s.size();i++) {
-        if (n+i >= t.size()) return 0;
-        if (t[n+i] != s[i]) return 0;
-    }
-    return 1;
-}
-
-int sol(int n) {
-    if (n == t.size()) return 0;
-    
-    int &ret = dp[n];
-    if (ret != -1) return ret;
-    
-    ret = INF;
-    for (auto &s : strs) {
-        if (check(n, s)) ret = min(ret, sol(n + s.size()));
-    }
-    return ++ret;
-}
-
-
-int solution(vector<string> _strs, string _t)
+int solution(vector<string> strs, string t)
 {
-    memset(dp, -1, sizeof(dp));
-    strs = _strs;
-    t = _t;
+    int answer = INF;
+    fill(dp, dp + 20004, INF);
+    
+    dp[0] = 0;
+    
+    for (int i=1;i<=t.size();i++) {
+        for (string &s : strs) {
+            int l = s.size();
+            if (i >= l && t.compare(i-l, l, s) == 0) {
+                dp[i] = min(dp[i], dp[i-l] + 1);
+            }
+        }
+        
+    }
     
 
-    return sol(0) >= INF ? -1 : sol(0);
+    
+
+    return dp[t.size()] == INF ? -1 : dp[t.size()];
 }
