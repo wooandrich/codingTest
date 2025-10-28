a1c6778e110bfa881ec4f1aa568ed39f5b8020c0
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2025-10-28 16:29:23 +0900

    [Gold V] Title: 기타 레슨, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/2343. 기타 레슨/기타 레슨.cc
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int n, m, arr[100004], ret = 1e9, l, r, mid;
+
+bool check(int target) {
+    int result = 0;
+    int cnt = 1;
+    for (int i=0;i<n;i++) {
+        if (result + arr[i] > target) {
+            result = arr[i];
+            cnt++;
+        } else {
+            result += arr[i];
+        }
+    }
+
+    return cnt <= m;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n >> m;
+
+    for (int i=0;i<n;i++) {
+        cin >> arr[i];
+        r += arr[i];
+        l = max(l, arr[i]);
+    }
+
+
+    while (l <= r) {
+        mid = (l + r) / 2;
+        if (check(mid)) {
+            ret = min(ret, mid);
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+
+    cout << ret << '\n';
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
