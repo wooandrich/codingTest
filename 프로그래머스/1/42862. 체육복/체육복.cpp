4c87fe5407d3dc832f0e274dc06af5db79dbad59
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2025-10-30 16:54:10 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int arr[34];
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+    int answer = 0;
+
+    fill(arr, arr+34, 1);
+
+    for (int i : reserve) arr[i]++;
+    for (int i : lost) arr[i]--;
+
+    for (int i=1;i<=n;i++) {
+        if (arr[i] > 1) {
+            if (arr[i-1] == 0) {
+                arr[i]--;
+                arr[i-1]++;
+            } else if (arr[i+1] == 0) {
+                arr[i]--;
+                arr[i+1]++;
+            }
+        }
+    }
+
+    for (int i=1;i<=n;i++) {
+        if (arr[i]) answer++;
+    }
+
+    return answer;
+}
