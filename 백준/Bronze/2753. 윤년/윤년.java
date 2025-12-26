f2d82d9c30538f9e4d50e209993c89aaa8d5c544
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2025-12-26 13:22:55 +0900

    [Bronze V] Title: 윤년, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(br.readLine());
+
+        if (a % 4 == 0 && a % 100 != 0 || a % 400 == 0) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        } 
+            
+    }
+}
+
