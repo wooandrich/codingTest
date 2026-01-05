117d0f04115cb109dfb3187b090f8281fb6f8814
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2026-01-05 15:53:53 +0900

    [Silver III] Title: 수열, Time: 236 ms, Memory: 23992 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,37 +1,33 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        List<Integer> ans = new ArrayList<>();
-
         int[] arr = new int[n];
-        int[] prefix = new int[n + 1];
+        int[] prefixSum = new int[n + 1];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
+            prefixSum[i+1] = prefixSum[i] + arr[i];
         }
 
-        for (int i = 0; i < n; i++) {
-            prefix[i+1] = prefix[i] + arr[i];
-        }
+        int ans = Integer.MIN_VALUE;
 
         for (int i = k; i <= n; i++) {
-            ans.add(prefix[i] - prefix[i-k]);
+            ans = Math.max(ans, prefixSum[i] - prefixSum[i - k]);
         }
 
-        System.out.println(Collections.max(ans));
-
-
-
+        System.out.println(ans);
 
 
 
@@ -39,3 +35,4 @@ public class Main {
 
     }
 }
+
