641725deb7a2904087cf475bc1f09ddf4457cf13
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2026-01-07 14:50:39 +0900

    [Silver II] Title: 유기농 배추, Time: 140 ms, Memory: 15856 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
+    static int[][] arr;
+    static int[][] visited;
+    static int n, m, k, t, ans;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        t = Integer.parseInt(br.readLine());
+
+        for (int a = 0; a < t; a++) {
+            ans = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            k = Integer.parseInt(st.nextToken());
+
+            arr = new int[n][m];
+            visited = new int[n][m];
+
+            for (int i = 0; i < k; i++) {
+                st = new StringTokenizer(br.readLine());
+                int y = Integer.parseInt(st.nextToken());
+                int x = Integer.parseInt(st.nextToken());
+
+                arr[y][x] = 1;
+            }
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (arr[i][j] == 1 && visited[i][j] == 0) {
+                        dfs(i ,j);
+                        ans++;
+                    }
+                }
+            }
+            System.out.println(ans);
+        }
+
+
+    }
+
+    static void dfs(int y, int x) {
+        visited[y][x] = 1;
+
+        for (int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+            if (arr[ny][nx] == 1 && visited[ny][nx] == 0) {
+                dfs(ny, nx);
+            }
+        }
+    }
+}
+
