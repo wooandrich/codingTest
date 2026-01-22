5c3d3950a16a4032d308782d483c63ad4467a5b5
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2026-01-22 13:21:46 +0900

    [Gold IV] Title: 연구소, Time: 284 ms, Memory: 54148 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,14 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Map;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
-class Main {
-    static final int[] dx = {-1, 1, 0, 0};
-    static final int[] dy = {0, 0, 1, -1};
-    static int[][] originalMap;
-    static int n,m;
-    static int maxSafeZone = Integer.MIN_VALUE;
+public class Main {
+
+    static int n;
+    static int m;
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
+    static int ans;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,122 +20,85 @@ class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        originalMap = new int[n][m];
+        int[][] arr = new int[n][m];
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
-                originalMap[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        dfs(0);
+        buildWall(arr, 0, 0,  0);
 
-        System.out.println(maxSafeZone);
+        System.out.println(ans);
 
 
 
 
     }
 
-    static void dfs(int wallCnt) {
-        // 벽이 3개가 되면 bfs시작
-        if (wallCnt == 3) {
-            bfs();
-            return;
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (originalMap[i][j] == 0) {
-                    originalMap[i][j] = 1;
-                    dfs(wallCnt + 1);
-                    originalMap[i][j] = 0;
+    static void buildWall(int[][] arr, int y, int x, int cnt) {
+        if (cnt == 3) {
+            int[][] visited = new int[n][m];
+            int[][] tempArr = new int[n][m];
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    tempArr[i][j] = arr[i][j];
                 }
             }
-        }
-    }
 
-    static void bfs() {
-        Queue<Node> q = new LinkedList<>();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (originalMap[i][j] == 2) {
-                    q.add(new Node(i, j));
-                }
-            }
-        }
-
-        // 원본 연구소를 바꾸지 않기 위해 카피맵 사용
-        int[][] copyMap = new int[n][m];
-
-        for (int i = 0; i < n; i++) {
-            copyMap[i] = originalMap[i].clone();
-        }
-
-        // bfs 탐색시작
-        while (!q.isEmpty()) {
-            Node now = q.poll();
-            int x = now.x;
-            int y = now.y;
-
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                // 빈칸을 경우에만 바이러스를 퍼트림
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
-                    if (copyMap[nx][ny] == 0) {
-                        q.add(new Node(nx, ny));
-                        copyMap[nx][ny] = 2;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (tempArr[i][j] == 2 && visited[i][j] == 0) {
+                        dfs(tempArr, visited, i, j);
                     }
                 }
             }
+            int result = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (tempArr[i][j] == 0) {
+                        result++;
+                    }
+                }
+            }
+            ans = Math.max(ans, result);
+            return;
         }
-
-
-        // safeZone 확인
-        funSafeZone(copyMap);
-
-
-
-
-
-
-
-    }
-
-    private static void funSafeZone(int[][] copyMap) {
-        int safeZone = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (copyMap[i][j] == 0) safeZone++;
+        int startI = y;
+        int startJ = x;
+        if (x == m) {
+            startI++;
+            startJ = 0;
+        }
+        for (int i = startI; i < n; i++) {
+            int k = (i == startI) ? startJ : 0;
+            for (int j = k; j < m; j++) {
+                if (arr[i][j] == 0) {
+                    arr[i][j] = 1;
+                    buildWall(arr, i, j+1, cnt + 1);
+                    arr[i][j] = 0;
+                }
             }
         }
-        maxSafeZone = Math.max(maxSafeZone, safeZone);
     }
 
+    static void dfs(int[][] tempArr, int[][] visited, int y, int x) {
+        visited[y][x] = 1;
 
-    // 큐에 좌표를 넣기 위해서
-    static class Node {
-        int x;
-        int y;
+        for (int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
 
-        public Node(int x, int y) {
-            this.x = x;
-            this.y = y;
+            if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+            if (tempArr[ny][nx] == 0 && visited[ny][nx] == 0) {
+                tempArr[ny][nx] = 2;
+                dfs(tempArr, visited, ny, nx);
+            }
         }
     }
 
-
-
-
-
 }
-
-
-
-
-
 
