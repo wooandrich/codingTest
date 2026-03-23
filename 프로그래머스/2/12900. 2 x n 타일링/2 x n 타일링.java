d46fb1113c7ad9c207b86f4c6cb0397770eda4a2
WooHyunLEE <101050466+wooandrich@users.noreply.github.com>
2026-03-23 21:52:53 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.41 ms, Memory: 54 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+            
+        int[] dp = new int[n+1];
+        
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for (int i=3;i<=n;i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1_000_000_007;
+        }
+        
+        return dp[n];
+    }
+}
